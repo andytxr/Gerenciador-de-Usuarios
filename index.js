913a81b6702aaa8d51455b4b999b23e3a366a6c3
decosawa <andreluiiz.gst@gmail.com>
2022-04-07 18:56:40 -0300

    Verificando gêneros

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
 var name = document.querySelector("#exampleInputName");
-var genderMale = document.querySelector("#exampleInputGenderM");
-var genderFem = document.querySelector("#exampleInputGenderF")
+var gender = document.querySelector("#form-user-create [name=gender]:checked");
 var birth = document.querySelector("#exampleInputBirth");
 var country = document.querySelector("#exampleInputCountry");
-var email = document.querySelector("#exampleInputEmail1");
-var password = document.querySelector("#exampleInputPassword1");
+var email = document.querySelector("#exampleInputEmail");
+var password = document.querySelector("#exampleInputPassword");
 var photo = document.querySelector("#exampleInputFile");
-var admin = doument.querySelector("#");
+var admin = document.querySelector("#exampleInputAdmin");
+
+//Quando usarmos [] em um querySelector quer dizer que estamos fazendo um filtro
+//Quando usarmos : é quando estamos procurando uma condição
+
+var fields = document.querySelectorAll("#form-usr-created [name]");
+fields.forEach(function(field, index){
+
+   if(field.name == "gender" && field.checked){
+       
+        console.log("foi :)");
+
+   }
+   else{
+       
+        console.log("nao foi :/");
+
+   }
+})
