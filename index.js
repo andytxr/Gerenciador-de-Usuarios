d72d340773929f10d17f05f5d870fc6818a028de
decosawa <andreluiiz.gst@gmail.com>
2022-04-08 14:34:28 -0300

    Adicionando funcionalidade de enviar o formulário e guardar dados no JSON

--- index.js
+++ index.js
@@ -12,19 +12,24 @@ var admin = document.querySelector("#exampleInputAdmin");
 
 var fields = document.querySelectorAll("#form-user-create [name]");
 var user = {};
+document.getElementById("form-user-create").addEventListener("submit", function(event){
+
+event.preventDefault();
 fields.forEach(function(field, index){
 
-   if(field.name == "gender" && field.checked){
-       
-        user[field.name]=field.value;
-
-   }
-   else{
+     if(field.name == "gender" && field.checked){
+         
+          user[field.name]=field.value;
   
-        console.log("nao foi :/");
-        user[field.name]=field.value;
+     }
+     else{
+    
+          user[field.name]=field.value;
+  
+     }
+  });
 
-   }
-});
+  console.log(user);
 
-console.log(user);
+})
+
