1b84154f92ab73bb90167eb98516b4d22017a88f
decosawa <andreluiiz.gst@gmail.com>
2022-04-11 16:32:12 -0300

    Utilizando LocalStorage para armazenamento de dados

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -102,9 +102,9 @@ class UserController{
 
         let users = [];
 
-        if(sessionStorage.getItem("users")){
+        if(localStorage.getItem("users")){
             
-            users=JSON.parse(sessionStorage.getItem("users"))
+            users=JSON.parse(localStorage.getItem("users"))
 
         }
 
@@ -235,7 +235,7 @@ class UserController{
         let users = this.getUsersStorage();
 
         users.push(data);
-        sessionStorage.setItem("users", JSON.stringify(users))
+        localStorage.setItem("users", JSON.stringify(users))
 
     }
 
